5e34989adf4c464d8c93bf77123ad7e866cf9b08
yeaminr07 <yeaminr07@gmail.com>
2023-06-17 03:39:52 +0600

    Component-basic

--- src/App.js
+++ src/App.js
@@ -1,82 +1,32 @@
-import { useState } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
-const finalSpaceCharacter = [
-  {
-    id: 'gary',
-    name: 'Gary Goodspeed',
-    thumb: '/images/gary.png'
-  },
-  {
-    id: 'cato',
-    name: 'Little Cato',
-    thumb: '/images/cato.png'
-  },
-  {
-    id: 'kvn',
-    name: 'KVN',
-    thumb: '/images/kvn.png'
-  },
-  {
-    id: 'mooncake',
-    name: 'Mooncake',
-    thumb: '/images/mooncake.png'
-  },
-  {
-    id: 'quinn',
-    name: 'Quinn Ergon',
-    thumb: '/images/quinn.png'
-  }
-]
+import BioData from './components/BioData';
+
 const App = () => {
-  const [characters, updateCharacters] = useState(finalSpaceCharacter);
-
-  function handleOnDragEnd(result) {
-    if (!result.destination)
-      return;
-
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    updateCharacters(items);
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Final Space Characters</h1>
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="characters">
-            {(provided) => (
-              <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-                {characters.map(({id, name, thumb}, index) => {
-                  return (
-                    <Draggable key={id} draggableId={id} index={index}>
-                      {(provided) => (
-                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                          <div className="characters-thumb">
-                            <img src={thumb} alt={`${name} Thumb`} />
-                          </div>
-                          <p>
-                            { name }
-                          </p>
-                        </li>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </header>
-      <p>
-        Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
-      </p>
+    <div className="App" >
+      <BioData
+        name="Don Yeamin"
+        email="yeaminr07@gmail.com"
+        linkedIn="linkedIn/wbsy"
+        mobile="01648798079"
+        github="https://github.com/yeamin07"
+        twitter="twitter/wbsy"
+        skills={['HTML', 'CSS', 'JavaScript', 'React']}
+        interests={['Artificial Intelligence', 'Robotics', 'Machine Learning', 'Software Engineering']}
+      />
+      <hr />
+      <BioData
+        name="Habib programmer"
+        email="habib101@gmail.com"
+        linkedIn="linkedIn/habib"
+        mobile="01648652469"
+        github="https://github.com/habibdeveloper12"
+        twitter="twitter/habib"
+        skills={['JavaScript', 'React', 'Nodejs', 'MongoBD']}
+        interests={['101', 'Whisky', 'Cigarette']}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
