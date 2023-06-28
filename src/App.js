161f31bd44e8efee7b2c267a67f5991fae5fb2c9
yeaminr07 <yeaminr07@gmail.com>
2023-06-28 08:40:43 +0600

    react-query Introduction with fake-API and create,delete functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,79 @@
+import { useState } from 'react'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import './App.css'
-import Counter from './components/Counter'
-import Theme from './components/Theme'
 
 const App = () => {
+    const [title, setTitle] = useState('');
+    const client = useQueryClient()
+
+    const fetchAllNote = async () => {
+        const response = await fetch(`http://localhost:4000/notes`);
+        return await response.json();
+    }
+
+    const { data, isLoading } = useQuery(['notes'], fetchAllNote)
+
+    const createNote = async () => {
+        const newNote = {
+            id: Date.now() + '',
+            title: title,
+        }
+        if(!title){
+            return alert(`Enter Something`)
+        }
+        const res = await fetch(`http://localhost:4000/notes`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(newNote)
+        })
+        setTitle('')
+        return await res.json()
+    }
+
+    const noteCreateMutation = useMutation(createNote, {
+        onSuccess: () => {
+            client.invalidateQueries(['notes'])
+        }
+    })
+
+    const removeHandler = async (id) => {
+        const res = await fetch(`http://localhost:4000/notes/${id}`, {
+            method: 'DELETE'
+        });
+        return await res.json()
+    }
+
+    const removeNoteMutation = useMutation((id) => removeHandler(id), {
+        onSuccess: () => {
+            client.invalidateQueries(['notes'])
+        }
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        noteCreateMutation.mutate()
+    }
+
+    if (isLoading) {
+        return <div>Loading.....</div>
+    }
+
     return (
         <div className='App'>
-            <Counter />
-            <br />
-            <hr />
-            <Theme />
+            <form onSubmit={handleSubmit}>
+                <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
+                <button type='submit'>Create Note</button>
+            </form>
+            <ul>
+                {data?.map(item => (
+                    <li key={item.id}>
+                        <span>{item.title}</span>
+                        <button onClick={() => removeNoteMutation.mutate(item.id)}>Remove button</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './store';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+const client = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+   <QueryClientProvider client={client} >
+     <ReactQueryDevtools initialIsOpen={false} />
+     <App />
+   </QueryClientProvider>
   </React.StrictMode>
 );
 
