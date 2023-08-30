ee1236aa1b50fa8e2883cbf47406e9497052dc3f
yeaminr07 <yeaminr07@gmail.com>
2023-08-31 00:33:59 +0600

    Tic-Tac-Toe-Game-with-react

---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+import "../App.css"
+
+
+const Square = ({val, chooseSquare}) => {
+  return (
+    <div className="square" onClick={chooseSquare}>
+        {val}
+    </div>
+  )
+}
+
+export default Square
