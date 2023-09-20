8a4452428d39361c35114230c783b26f432e9310
yeaminr07 <yeaminr07@gmail.com>
2023-09-20 22:05:14 +0600

    GPT3-full-responsive-website-css-based-react-project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css'
-import Router from './router/Router';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
+import { CTA, Brand, Navbar } from './components'
 
-const App = () => {    
+const App = () => {
     return (
-        <div className='App'>   
-            <Router />
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
